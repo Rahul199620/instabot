0b8ea1e8b490156ed4e09c5d53f754d0a4e5b4c5
Rahul199620 <cse.rahul01@gmail.com>
2017-07-05 22:25:07 +0530

    get own ost is modified

--- get_ownpost.py
+++ get_ownpost.py
@@ -1,4 +1,5 @@
 #get_own_post is intialized
+
 from constant import Access_Token,BASE_URL
 import requests
 
@@ -7,3 +8,15 @@ def get_own_post():
   request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (Access_Token)
   print 'GET request url : %s' % (request_url)
   own_media = requests.get(request_url).json()
+
+  if own_media['meta']['code']== 200:
+      if len(own_media['data']):
+          return own_media['data'][0]['id']
+      else:
+          print "no post is found"
+
+  else:
+      print"error 404 found"
+
+
+
--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 from selfinfo import self_info
 from getuser_id import get_user_id
 from get_userinfo import get_user_info
+from get_ownpost import get_own_post
 
 #fetching our own information
 #self_info()
-insta_username=raw_input('enter your name')
+#insta_username=raw_input('enter your name')
 
 #get_user_id(insta_username)
 
-get_user_info(insta_username)
+#get_user_info(insta_username)
+#function call getownpost
+r=get_own_post()
+print r
 
 
