2b96693f939a67f873cf7b4ac4b31c60eebdd79e
Rahul199620 <cse.rahul01@gmail.com>
2017-07-05 20:14:11 +0530

    get user id is modifie and function call in main.py

--- getuser_id.py
+++ getuser_id.py
@@ -1,11 +1,25 @@
 #get user id file is created
 #import global variable
-from constant import BASE_URL,Access_Token
+from constant import BASE_URL ,Access_Token
+#import requests library
 import requests
 
-def get_userid(insta_username):
-    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username,Access_Token)
-    print( 'get request_url %s')%(request_url)
-    user_info=requests.get(request_url).json()
+#declaring function get user id
+def get_user_id(insta_username):
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, Access_Token)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            return user_info['data'][0]['id']
+        else:
+            return None
+    else:
+        print 'Status code other than 200 received!'
+        exit()
+
+
+
 
 
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #mainfile is intialized
 from selfinfo import self_info
+from getuser_id import get_user_id
 
 #fetching our own information
-self_info()
+#self_info()
+insta_username=raw_input('enter your name')
+
+get_user_id(insta_username)
+
