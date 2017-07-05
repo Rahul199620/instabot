6cff492581b53d5a8e840da091903c8294769434
Rahul199620 <cse.rahul01@gmail.com>
2017-07-05 22:05:57 +0530

    functtion get own post is created

--- get_ownpost.py
+++ get_ownpost.py
@@ -1,1 +1,9 @@
 #get_own_post is intialized
+from constant import Access_Token,BASE_URL
+import requests
+
+#function is declared
+def get_own_post():
+  request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (Access_Token)
+  print 'GET request url : %s' % (request_url)
+  own_media = requests.get(request_url).json()
