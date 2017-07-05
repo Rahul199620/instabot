7869fe7867a891a23d7d5d7eb5fac5e942cf8260
Rahul199620 <cse.rahul01@gmail.com>
2017-07-05 21:02:52 +0530

    main.py and get user info is modified

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #mainfile is intialized
 from selfinfo import self_info
 from getuser_id import get_user_id
+from get_userinfo import get_user_info
 
 #fetching our own information
 #self_info()
 insta_username=raw_input('enter your name')
 
-get_user_id(insta_username)
+#get_user_id(insta_username)
+
+get_user_info(insta_username)
+
 
