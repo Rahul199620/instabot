600ace6d0b40da6d446b13a591f0af53b4bebe09
Rahul199620 <cse.rahul01@gmail.com>
2017-07-05 20:31:20 +0530

    create a function for access user info

--- get_userinfo.py
+++ get_userinfo.py
@@ -1,4 +1,12 @@
 #get user info file is created
-
-def get_info():
-    #logic of code
+#import global variable
+from constant import Access_Token , BASE_URL
+import requests
+def get_user_info(insta_username):
+  user_id = get_user_id(insta_username)
+  if user_id == None:
+    print 'User does not exist!'
+    exit()
+  request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, Access_Token)
+  print 'GET request url : %s' % (request_url)
+  user_info = requests.get(request_url).json()
