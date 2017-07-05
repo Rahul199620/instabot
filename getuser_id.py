ba3a140526f5e81f037059140fd1a310c5ce1756
Rahul199620 <cse.rahul01@gmail.com>
2017-07-05 19:16:06 +0530

    request the userid

--- getuser_id.py
+++ getuser_id.py
@@ -1,3 +1,11 @@
 #get user id file is created
+#import global variable
+from constant import BASE_URL,Access_Token
+import requests
 
 def get_userid(insta_username):
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username,Access_Token)
+    print( 'get request_url %s')%(request_url)
+    user_info=requests.get(request_url).json()
+
+
