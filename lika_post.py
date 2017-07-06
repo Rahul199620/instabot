cd452c458347a8572892c7dde434e3323fcd15c1
Rahul199620 <cse.rahul01@gmail.com>
2017-07-06 18:34:14 +0530

    post id file is modified

--- lika_post.py
+++ lika_post.py
@@ -1,2 +1,14 @@
 #like a post file is created
-def like_a_post():
+import requests
+from
+def like_a_post(insta_username):
+    media_id = get_post_id(insta_username)
+    request_url = (BASE_URL + 'media/%s/likes') % (media_id)
+    payload = {"access_token": APP_ACCESS_TOKEN}
+    print 'POST request url : %s' % (request_url)
+    post_a_like = requests.post(request_url, payload).json()
+
+    if post_a_like['meta']['code'] == 200:
+        print 'Like was successful!'
+    else:
+        print 'Your like was unsuccessful. Try again!'
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 #mainfile is intialized
-from selfinfo import self_info
-from getuser_id import get_user_id
-from get_userinfo import get_user_info
+#from selfinfo import self_info
+#from getuser_id import get_user_id
+#from get_userinfo import get_user_info
+from post_id import get_post_id
 
-from get_ownpost import get_own_post
-from get_user_post import get_user_post
+#from get_ownpost import get_own_post
+#from get_user_post import get_user_post
 
 #fetching our own information
 #self_info()
-insta_username=raw_input('enter your name')
-get_user_post(insta_username)
+#insta_username=raw_input('enter your name')
+#get_user_post(insta_username)
+insta_usename=raw_input('enter your name')
+resul=get_post_id(insta_usename)
+print resul
 
 
 
-get_user_id(insta_username)
+#get_user_id(insta_username)
 
 #get_user_info(insta_username)
 #function call getownpost
